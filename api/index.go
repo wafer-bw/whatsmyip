483745078540e03f74e520652dd09ebf9b6aea69
wafer-bw <ben.jz.wakeford@gmail.com>
2020-09-21 17:18:17 -0400

    add content type response headers

--- api/index.go
+++ api/index.go
@@ -26,13 +26,16 @@ func resolver(request *http.Request) *spec.IPReply {
 	return &spec.IPReply{Ip: ip}
 }
 
-func marshaller(request *http.Request, reply *spec.IPReply) (body []byte, err error) {
+func marshaller(writer http.ResponseWriter, request *http.Request, reply *spec.IPReply) (body []byte, err error) {
 	switch request.Header.Get("accept") {
 	case "application/protobuf":
+		writer.Header().Set("Content-Type", "application/protobuf")
 		return proto.Marshal(reply)
 	case "application/json":
+		writer.Header().Set("Content-Type", "application/json")
 		return json.Marshal(reply)
 	default:
+		writer.Header().Set("Content-Type", "text/plain")
 		return []byte(reply.Ip), nil
 	}
 }
@@ -40,7 +43,7 @@ func marshaller(request *http.Request, reply *spec.IPReply) (body []byte, err er
 // Handler responds with the IP address of the request
 func Handler(writer http.ResponseWriter, request *http.Request) {
 	log.Println(*request)
-	body, err := marshaller(request, resolver(request))
+	body, err := marshaller(writer, request, resolver(request))
 	if err != nil {
 		log.Println(err)
 		http.Error(writer, err.Error(), 500)
--- api/index_test.go
+++ api/index_test.go
@@ -4,6 +4,7 @@ import (
 	"io"
 	"io/ioutil"
 	"log"
+	"net/http"
 	"net/http/httptest"
 	"os"
 	"testing"
@@ -18,7 +19,7 @@ func TestMain(m *testing.M) {
 	os.Exit(m.Run())
 }
 
-func mockRequest(method string, url string, headers map[string]string, body io.Reader) ([]byte, int, error) {
+func mockRequest(method string, url string, headers map[string]string, body io.Reader) ([]byte, http.Header, int, error) {
 	recorder := httptest.NewRecorder()
 	request := httptest.NewRequest("GET", url, nil)
 	for key, val := range headers {
@@ -27,28 +28,31 @@ func mockRequest(method string, url string, headers map[string]string, body io.R
 	router := GetRouter()
 	router.ServeHTTP(recorder, request)
 	result, err := ioutil.ReadAll(recorder.Body)
-	return result, recorder.Result().StatusCode, err
+	return result, recorder.Result().Header, recorder.Result().StatusCode, err
 }
 
-func TestHandlerOk(t *testing.T) {
-	body, status, err := mockRequest("GET", url, nil, nil)
+func TestHandlerOkText(t *testing.T) {
+	body, headers, status, err := mockRequest("GET", url, nil, nil)
 	require.NoError(t, err)
+	require.Equal(t, "text/plain", headers.Get("content-type"))
 	require.Equal(t, "192.0.2.1", string(body))
 	require.Equal(t, 200, status)
 }
 
 func TestHandlerOkJson(t *testing.T) {
-	headers := map[string]string{"Accept": "application/json"}
-	body, status, err := mockRequest("GET", url, headers, nil)
+	requestHeaders := map[string]string{"Accept": "application/json"}
+	body, headers, status, err := mockRequest("GET", url, requestHeaders, nil)
 	require.NoError(t, err)
+	require.Equal(t, "application/json", headers.Get("content-type"))
 	require.Equal(t, "{\"ip\":\"192.0.2.1\"}", string(body))
 	require.Equal(t, 200, status)
 }
 
 func TestHandlerOkProto(t *testing.T) {
-	headers := map[string]string{"Accept": "application/protobuf"}
-	body, status, err := mockRequest("GET", url, headers, nil)
+	requestHeaders := map[string]string{"Accept": "application/protobuf"}
+	body, headers, status, err := mockRequest("GET", url, requestHeaders, nil)
 	require.NoError(t, err)
+	require.Equal(t, "application/protobuf", headers.Get("content-type"))
 	require.Equal(t, "\n\t192.0.2.1", string(body))
 	require.Equal(t, 200, status)
 }
