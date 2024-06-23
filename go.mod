fe06bc99591a9b75d4a0bc5692c9ed16a57fce93
Ben Wakeford <ben.jz.wakeford@gmail.com>
2024-06-22 20:22:02 -0400

    try to make web page more snappy

--- go.mod
+++ go.mod
@@ -1,8 +1,6 @@
 module github.com/wafer-bw/whatsmyip
 
-go 1.21
-
-toolchain go1.21.7
+go 1.22.4
 
 require (
 	github.com/golangci/golangci-lint v1.59.0
--- public/js/setIp.js
+++
@@ -1,9 +0,0 @@
-fetch("/api", { headers: { 'accept': 'application/json' } })
-    .then(response => response.json())
-    .then(data => {
-        document.getElementById("ip").innerHTML = data["ip"]
-    }).catch(err => {
-        msg = "There was a problem getting your IP address"
-        document.getElementById("ip").innerHTML = msg
-        console.error(err)
-    })
